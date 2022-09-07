223958edd50be87152c5c3e9f2d06ec75adeb4d2
Юлия Сенина <JuLKa9449@yandex.ru>
2022-09-07 08:32:17 +0300

    commit

--- src/zombie.js
+++ src/zombie.js
@@ -1,8 +1,8 @@
 import Character from "./character";
 
 export default class Zombie extends Character {
-  constructor(name, type) {
-    super(name, type = "Zombie");
+  constructor(name) {
+    super(name, "Zombie");
     this.attack = 40;
     this.defence = 10;
   };
